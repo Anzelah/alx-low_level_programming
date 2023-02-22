de4790bb6adce108c07a4a4f8b78842ebb736d35
Anzelah <jepkoechanzellah@gmail.com>
2023-02-21 17:16:42 -0800

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,11 +8,19 @@
  * -1 and prints - if n is less than zero
 */
 
-int print_sign(int)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	} else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	} else
+	{
+		_putchar ('-');
+		return (-1);
+	}
 }
