b686cfeaecfd09225e29a5cfebd2e3f710d3a343
Anzelah <jepkoechanzellah@gmail.com>
2023-02-27 13:12:42 -0800

    characters in string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,22 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int d = 0;
+	char *c = str;
+	int j;
 
-	for (int i = 0; str[i] != '\0'; i++)
+	while (*c != '\0')
 	{
-		_putchar(str[i]);
+		c++;
+		i++;
 	}
+	d = i - 1;
+	for (j = 0; j <= d; j++)
+	{
+		if (j % 2 == 0)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
 }
