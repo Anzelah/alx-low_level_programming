1aa5280ccfd981ccc26d584006084b67c4bfaeb9
Anzelah <jepkoechanzellah@gmail.com>
2023-03-02 07:00:26 -0800

    compare strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse contents of an array
+ * @n: elements inside the array
+ * @a: array
+ *
+ * Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (i = 10; i <= 0; i--)
+	{
+		if (i < n)
+		i--;
+	}
+}
