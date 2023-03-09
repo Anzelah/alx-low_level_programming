53b60d9685689172f5b9e9ba0c1c78c6a6d535c6
Anzelah <jepkoechanzellah@gmail.com>
2023-03-09 05:49:29 -0800

    print length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,12 @@
 
 int _strlen_recursion(char *s)
 {
-	char c = 0;
+	int i = 0;
 
-	if (s[c] != '\0')
+	if (*s)
 	{
-		_putchar(*s);
-		_strlen_recursion(s - 1);
+		i++;
+		i += _strlen_recursion(s + 1);
 	}
+	return (i);
 }
