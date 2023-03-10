42efd504dca8815f4699901b066e9fb609647e2d
Anzelah <jepkoechanzellah@gmail.com>
2023-03-10 06:25:54 -0800

    create a static library

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination where content is to be copied
+ * @src: data source to be copied
+ * @n: number of bytes to be copied
+ *
+ * Return: character
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncat - concecntates two strings
+ * @src: string
+ * @dest: string
+ * @n: bytes to be used
+ *
+ * Return: string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+}
