c7b2eda8c08cd385530c99ddcb8f75ab93f338b5
Anzelah <jepkoechanzellah@gmail.com>
2023-03-11 15:36:45 -0800

    add positive integers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc == 1)
 	{
@@ -21,10 +21,15 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (!(*(argv[i]) >= 48 && *(argv[i]) <= 57))
+			j = 0;
+			while (*(argv[i] + j))
 			{
-				printf("Error\n");
-				return (1);
+				if (!(*(argv[i] + j) >= 48 && *(argv[i] + j) <= 57))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				j++;
 			}
 
 			sum += atoi(argv[i]);
