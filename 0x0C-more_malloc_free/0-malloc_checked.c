bb380810bedfc70b97185fec76ad5126de0d6946
Anzelah <jepkoechanzellah@gmail.com>
2023-03-16 17:07:49 -0700

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: array value
+ *
+ * Return: 98 if it fails
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *arr;
+
+	arr = (int *) malloc(b * sizeof(int));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	int i;
+	for (i = 0; i < b; i++)
+	{
+		arr[i] = i + 1;
+	}
+	free(arr);
+
+	return (0);
+}
