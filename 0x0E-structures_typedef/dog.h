e44d2d18162028ae5da7ec9196b6d201cddafe1d
Anzelah <jepkoechanzellah@gmail.com>
2023-03-20 12:04:24 -0700

    data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * struct dog - a new data type
+ * @name: its name
+ * @age: its age
+ * @owner: the owner
+ *
+ * Return: always (0)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* MAIN_H */
