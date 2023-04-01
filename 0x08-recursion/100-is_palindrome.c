8e4382ec6e67ae8eb3d50921209c82aec145d4e1
Anzelah <jepkoechanzellah@gmail.com>
2023-04-01 05:36:18 -0700

    compare two strings

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,12 +12,12 @@ int is_palindrome(char *s)
 {
 	int i = 0;
 	int n = '\0' - 1;
-	
+
 	if (i >= n)
 		return (1);
 
 	if (s[i++] == s[n--])
-		return (is_palindrome (s + 1));
+		return (is_palindrome(s + 1));
 	else
 		return (0);
 }
