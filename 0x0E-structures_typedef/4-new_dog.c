c906c10897e1da6e039f838d41928dbcc51c5d3f
Anzelah <jepkoechanzellah@gmail.com>
2023-04-04 05:05:16 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,14 +32,17 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	int len = 0;
 
-	i = 0;
-
-	while (src[i] != '\0')
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+
 	dest[i] = '\0';
 
 	return (dest);
