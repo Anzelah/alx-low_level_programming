47421c8ac5b6ad68f0bbdb40cae5056c83b6e8bb
Anzelah <jepkoechanzellah@gmail.com>
2023-04-20 05:41:41 -0700

    calculator function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "3-calc.h"
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div}
+		{"%", op_mod},
+			{NULL, NULL}
+	};
+	int i;
+
+/**
+ * get_op_func - selects correct function to perform operation
+ * asked by the user
+ * @s: operator passed as argument to the program
+ *
+ * Return: a pointer to the function that corresponds
+ * to the operator given as parameter
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].func);
+		i++;
+	}
+	return (NULL)
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - the entry point
+ *
+ * Return: result of the operation
+ */
+
+int main(void)
+{
+	int (*get_op_func(char *s))(int, int);
+
+	int ch;
+	char *s;
+
+	ch = atoi(s);
+
+	if (ch > 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
