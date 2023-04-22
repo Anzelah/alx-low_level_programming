43b18d4799dda0b14173a46155a749eb7eac368c
Anzelah <jepkoechanzellah@gmail.com>
2023-04-22 05:48:30 -0700

    rewrote previous string

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,10 +34,9 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				str = va_arg(list, char *);
-				if (str == NULL)
-					printf("(nil");
-				else
-					printf("%s", str);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
 				break;
 			default:
 				i++;
