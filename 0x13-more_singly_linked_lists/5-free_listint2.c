d9608b9bb314074c63edfea9c91345881b67c097
Anzelah <jepkoechanzellah@gmail.com>
2023-05-01 07:38:12 -0700

    first trial

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free linked list wwhie setting head to NULL
+ * @head: double pointer to linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
