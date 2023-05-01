ac16b4f7eae6b1578164b3ed30462accf2dc8370
Anzelah <jepkoechanzellah@gmail.com>
2023-05-01 07:55:18 -0700

    return nothing if head itself or head to node is NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head != NULL)
+	if (head == NULL || *head  == NULL)
+		return;
+	
+	while(*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
-	else
-		return;
-
 	*head = NULL;
 }
