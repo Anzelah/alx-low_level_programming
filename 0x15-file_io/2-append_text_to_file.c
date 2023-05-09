6ca2f52e6e5b83d6e0667d50c1aea4dc8b1c3721
Anzelah <jepkoechanzellah@gmail.com>
2023-05-09 13:20:09 -0700

    check if filename is NULL

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,10 +15,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	int len, bts = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
