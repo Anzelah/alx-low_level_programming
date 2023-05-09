3decd97900ab23f5f8001428f83524624fbc8c70
Anzelah <jepkoechanzellah@gmail.com>
2023-05-09 08:38:20 -0700

    find the length of the string for use inside write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,17 +12,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-
-	fd = open(filename, O_CREAT|O_TRUNC, S_IRUSR | S_IWUSR);
+	int fd, bts = 0;
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_CREAT| O_RDWR | O_TRUNC, 0600);
+
 	if (text_content != NULL)
 	{
-		write(STDOUT_FILENO, text_content, fd);
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+		bts = write(fd, text_content, len);
+		if (bts == -1 || fd == -1)
+			return (-1);
 	}
 	close (fd);
+
 	return (1);
 }
