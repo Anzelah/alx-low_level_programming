915d5cc56bd392c696a61342eed5b0096fd9ae25
Anzelah <jepkoechanzellah@gmail.com>
2023-05-09 10:42:52 -0700

    betty update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT| O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (text_content != NULL)
 	{
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 		if (bts == -1 || fd == -1)
 			return (-1);
 	}
-	close (fd);
+	close(fd);
 
 	return (1);
 }
