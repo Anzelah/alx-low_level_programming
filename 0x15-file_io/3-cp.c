b73586c2bc3db82521505a40311820767d8e619c
Anzelah <jepkoechanzellah@gmail.com>
2023-05-10 04:05:06 -0700

    first trial

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the entry point
+ * @argc: counts the number of arguments
+ * @argv: contains arguments passed onto the program
+ *
+ * Return: 0 on success, -1 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd, fp;
+	int brts, bts;
+	char *buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+		return (-1);
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit (97);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	bts = read(fd, buffer, 1024);
+
+	if (fd == -1 || bts == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit (98);
+	}
+	close(fd);
+
+	fp = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	brts = write(fd, buffer, bts);
+
+	if (fp == -1 || brts == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", (argv[2]));
+		exit (99);
+	}
+
+	if (close(fp) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (fp));
+		exit (100);
+	}
+	
+	close(fp);
+	free(buffer);
+
+	return (0);
+}
