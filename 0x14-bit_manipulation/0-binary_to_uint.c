0ab63300311d80d84164220fb2fe803c80cdeda2
Anzelah <jepkoechanzellah@gmail.com>
2023-05-25 12:57:50 -0700

    change binary to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - convert binary number to an unsigned integer
+ * @b: points to a string of 0 and 1 chars
+ *
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int decimal = 0;
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] > '1' || b[i] < '0')
+			return (0);
+		decimal = 2 * decimal + (b[i] - '0');
+	}
+	return (decimal);
+}
