7b292b9eb7f7acac1c20d5e79ad9ca7bc2f9817e
Anzelah <jepkoechanzellah@gmail.com>
2023-06-15 13:57:13 -0700

    handle index being larger than current umber of nodes

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newnode = malloc(sizeof(dlistint_t));
 	if (!newnode)
 		return (NULL);
-
 	newnode->n = n;
 	if (*h == NULL) /* if lnked list is empty */
 	{
@@ -40,9 +39,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-	if (temp->next == NULL)
+	if (temp->next == NULL && i < idx - 1)
 	{
-		return (add_dnodeint_end(h, n));
+		free(newnode);
+		return (NULL);
 	}
 	newnode->next = temp->next;
 	newnode->prev = temp;
